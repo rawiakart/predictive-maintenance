7eb94402fe124516c7674eb7a9b35d60537f4724
Rawia kh <rawiakart@gmail.com>
2025-12-09 09:13:35 +0100

    ajout de appel à main depuis C# app

--- main.py
+++ main.py
@@ -91,3 +91,7 @@ def predict_rul_all(data: list[EquipmentData]):
 
     return jsonable_encoder(predictions)
     
+    
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
